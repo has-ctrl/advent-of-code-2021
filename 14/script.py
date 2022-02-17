09d309feb5082f108ab690f9e37abf6150b7283d
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2022-02-17 19:04:18 +0100

    Day 14 - Part Two
    Thought of a similar approach as day 6 to just keep track of the pairs. Now the script completes in less than a second.

--- 14/script.py
+++ 14/script.py
@@ -33,7 +33,7 @@ def one(data: list, mapping: dict, limit: int = 10) -> int:
     return max(freqs) - min(freqs)
 
 
-def two(data: list) -> int:
+def two(data: list, mapping: dict, limit: int = 40) -> int:
     """
     Apply 40 steps of pair insertion to the polymer template and find the most and least common elements in the result.
     What do you get if you take the quantity of the most common element and subtract the quantity of the least common
@@ -41,8 +41,45 @@ def two(data: list) -> int:
 
     Practically the first, but needs to be more efficient.
     """
-    return 0
+    def recursive_count(template: dict, count: int = 1) -> dict:
+        """
+        Recursively keep track of how many of each pairing are present.
+        """
+        res = {}
+        for (el, next_el), freq in template.items():
+            new = mapping.get(el + next_el, 0)
+            res[(el, new)] = res.get((el, new), 0) + freq
+            res[(new, next_el)] = res.get((new, next_el), 0) + freq
+
+        if count == limit:
+            return res
+        else:
+            return recursive_count(res, count+1)
+
+    def calc_freqs(template: dict) -> int:
+        """
+        Calculate the frequency of each individual element and calculate the difference between the most common and
+        least common elements.
+        """
+        # Add one to the first and last element before dividing by 2 later.
+        res = {
+            data[0]: 1,
+            data[-1]: 1,
+        }
+        for (el, next_el), freq in template.items():
+            res[el] = (res.get(el, 0) + freq)
+            res[next_el] = (res.get(next_el, 0) + freq)
+
+        return int((max(res.values()) - min(res.values())) / 2)
+
+    # Create frequency dictionary with template pairs.
+    freq_dict = {}
+    for tup in zip(data, data[1:]):
+        freq_dict[tup] = freq_dict.get(tup, 0) + 1
+
+    final_template = recursive_count(freq_dict)
+    return calc_freqs(final_template)
 
 
 print(f"1. {one(polymer_template, pair_insertion)}")
-print(f"2. {two(polymer_template)}")
+print(f"2. {two(polymer_template, pair_insertion)}")
