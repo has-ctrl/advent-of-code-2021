7b6e9bd1a19a2ffb34a11763eebf0b7c7af75f4c
has-ctrl <57541155+has-ctrl@users.noreply.github.com>
2022-08-17 22:13:48 +0200

    Day 15
    Integrated priority queue (heapq) and wrote update_map() method for part 2.

--- 15/script.py
+++ 15/script.py
@@ -1,102 +1,90 @@
+from collections import defaultdict
+from heapq import heappop, heappush
 import numpy as np
-from dataclasses import dataclass
-from typing import Any
-
-
-@dataclass
-class Node:
-    """
-    Default Node in the graph.
-    """
-    x_position: int
-    y_position: int
-    parent: Any  # Node datatype
-    g: int = 0
-    h: int = 0
-    f: int = 0
-
-    def __lt__(self, other):
-        return self.f > other.f
-
-    def __eq__(self, other):
-        return self.x_position == other.x_position and self.y_position == other.y_position
+import math
 
 
 def calculate_lowest_risk(data) -> int:
     """
-    Use the A* search algorithm to retrieve the shortest path and associated (lowest) risk.
+    Use the A* search algorithm to retrieve the shortest path and associated (lowest) risk (with priority queue).
     """
 
-    def is_valid_node(n: Node) -> bool:
+    def is_valid_node(_x: int, _y: int) -> bool:
         """
         Check whether the proposed node is valid.
         """
-        return min_x <= n.x_position <= max_x and min_y <= n.y_position <= max_y
+        return min_x <= _x <= max_x and min_y <= _y <= max_y
 
     min_x = min_y = 0
     max_x = max_y = data.shape[0] - 1
 
-    start_node = Node(min_x, min_y, None)
-    end_node = Node(max_x, max_y, None)
+    start_node = (min_x, min_y)
+    end_node = (max_x, max_y)
 
-    open_list = [start_node]
-    closed_list = []
+    risk_dict = defaultdict(lambda: math.inf)
+    risk_dict[start_node] = 0
+
+    open_list = []
+    heappush(open_list, (0, start_node))
+
+    unvisited = {(x, y) for x in range(len(data)) for y in range(len(data))}
 
     # Loop until the end node has been found.
-    while open_list:
+    while end_node in unvisited:
+        current_risk, current = heappop(open_list)
 
-        # Get the current node (lowest f score)
-        open_list.sort(reverse=True)
-        current_node = open_list[0]
-        current_index = 0
+        # Double check.
+        if current not in unvisited:
+            continue
 
-        # Remove current node from open list and add to closed list.
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the end node.
-        if current_node == end_node:
-            return current_node.g  # reconstruct_path(current_node)
-
-        # Generate potential paths (children nodes).
         child_nodes = []
         for x_pos_change, y_pos_change in [(-1, 0), (1, 0), (0, -1), (0, 1)]:  # left, right, down, up.
 
+            x, y = current
+
             # New node positions.
-            x_pos = current_node.x_position + x_pos_change
-            y_pos = current_node.y_position + y_pos_change
+            x_pos = x + x_pos_change
+            y_pos = y + y_pos_change
 
             # Append new node to child nodes if it is valid.
-            new_node = Node(x_pos, y_pos, current_node)
-            if is_valid_node(new_node):
-                child_nodes.append(new_node)
+            if is_valid_node(x_pos, y_pos):
+                child_nodes.append((x_pos, y_pos))
 
-        # Loop through child nodes.
         for child_node in child_nodes:
 
-            to_consider: bool = True
-
             # Only consider child nodes not in closed list.
-            if child_node in closed_list:
+            if child_node not in unvisited:
                 continue
 
-            child_node.g = current_node.g + data[current_node.y_position][current_node.x_position]
-            child_node.h = abs(child_node.x_position - end_node.x_position) + \
-                           abs(child_node.y_position - end_node.y_position)
-            child_node.f = child_node.g + child_node.h
+            # Only update (cumulative) risk only if it is lower.
+            child_risk = min(
+                risk_dict[child_node],
+                risk_dict[current] + data[child_node[1]][child_node[0]]
+            )
 
-            # Only consider child nodes for which the cost (g) is lower.
-            if child_node in open_list:
-                for open_node in open_list:
-                    if child_node == open_node and child_node.g > open_node.g:
-                        to_consider = False
-                        break
+            risk_dict[child_node] = child_risk
+            heappush(open_list, (child_risk, child_node))
 
-            if to_consider:
-                open_list.append(child_node)
+        unvisited.remove(current)
+
+    return risk_dict[end_node]
 
 
-np_data = np.array([[int(bit) for bit in bit_str] for bit_str in np.loadtxt("data.txt", dtype=str)])
+def update_map(data) -> np.ndarray:
+    """
+    Update the risk for a 5x5 map.
+    """
+    max_x, max_y = data.shape
+    risk_map = np.empty((max_x * 5, max_y * 5))
+
+    for y_index, y in enumerate(risk_map):
+        for x_index, x in enumerate(y):
+            risk = data[y_index % max_y][x_index % max_x]  # The risk value in the regular map (data).
+
+            # Update the risk value (1>2, 2>3, ..., 9>1).
+            risk_map[y_index][x_index] = (risk + ((y_index // max_y) + (x_index // max_x)) - 1) % 9 + 1
+
+    return risk_map
 
 
 def one(data: np.ndarray) -> int:
@@ -106,4 +94,17 @@ def one(data: np.ndarray) -> int:
     return calculate_lowest_risk(data)
 
 
+def two(data: np.ndarray) -> int:
+    """
+    Using the full map, what is the lowest total risk of any path from the top left to the bottom right?
+    """
+    return int(one(update_map(data)))
+
+
+np_data = np.array([[int(bit) for bit in bit_str] for bit_str in np.loadtxt("data.txt", dtype=str)])
+
+
 print(f"1. {one(np_data)}")
+print(f"2. {two(np_data)}")
+
+
